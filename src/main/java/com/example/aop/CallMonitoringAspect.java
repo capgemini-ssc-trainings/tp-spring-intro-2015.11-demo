4a0bb146de93683a6506ed0aa25c49e23e9f6536
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2015-11-11 23:16:29 +0100

    Basic AOP added

---
+++ src/main/java/com/example/aop/CallMonitoringAspect.java
@@ -0,0 +1,39 @@
+package com.example.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class CallMonitoringAspect {
+    Logger logger = org.slf4j.LoggerFactory.getLogger(CallMonitoringAspect.class);
+
+    @Pointcut("execution(* com.example..service.BookService.*(..))")
+    public void serviceMethods() {
+
+    }
+
+    @Around("serviceMethods()")
+    public Object monitor(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        this.logger.info("Monitoring of " + proceedingJoinPoint.toString() + " started");
+
+        StopWatch sw = new StopWatch(proceedingJoinPoint.toString());
+
+        try {
+            sw.start(proceedingJoinPoint.toShortString());
+            return proceedingJoinPoint.proceed();
+
+        } finally {
+            sw.stop();
+            this.logger.info(sw.prettyPrint());
+            this.logger.info("Monitoring of " + proceedingJoinPoint.toString() + " finished after "
+                    + sw.getLastTaskTimeMillis() + " ms");
+        }
+    }
+}
